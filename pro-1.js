4085c8448163293b871b94e29850df81e71e0e63
Ramla Jama <75176930+ramla199@users.noreply.github.com>
2023-11-05 23:53:32 +0000

    "largestPalindromeOf3"

--- pro-1.js
+++ pro-1.js
@@ -15,6 +15,7 @@
 // Multiples of 5= 5,10,15,20,25,30...etc
 // Multiples of 9= 9,18,27,36,45,54...etc
 
+//// MORE NOTES
 // Every number has at least two factors, which are 1 and itself.
 // A number can have an infinite number of multiples, but only a finite number of factors.
 // Factors and multiples can be used to find the patterns and symmetries in nature, such as the arrangement of petals in flowers and the formation of crystals.
@@ -43,7 +44,7 @@
 //Q- If we list all the natural numbers below  10 that are multiples of 3 or 5, we get 3,5,6 and 9. the sum of these multiples is 23.
 // find the sum of all the multiples of 3 or 5 below 1000
 
-// A-
+// Ans
 let sum = 0 ,num = 0;
 
 while(num < 1000) {
